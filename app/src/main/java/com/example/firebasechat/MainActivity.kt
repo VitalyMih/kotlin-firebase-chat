801eaf4f8afba8a78e167c585c3af62e763c6ad0
VitalyMih <vitalymihale@yandex.ru>
2023-04-26 17:03:02 +0300

    Add sign out button + avatar and name of the user

--- app/src/main/java/com/example/firebasechat/MainActivity.kt
+++ app/src/main/java/com/example/firebasechat/MainActivity.kt
@@ -1,25 +1,47 @@
 package com.example.firebasechat
 
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.example.firebasechat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-   private  lateinit var databaseRef: DatabaseReference
+    private lateinit var databaseRef: DatabaseReference
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
         databaseRef = Firebase.database.getReference("messages")
+        setUpAvatar()
         dataChangeListener()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.sign_out) {
+            auth.signOut()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun dataChangeListener() = with(binding) {
@@ -36,5 +58,18 @@ class MainActivity : AppCompatActivity() {
 
     fun onSendMessageClick(view: View) = with(binding) {
         databaseRef.setValue(messageText.text.toString())
+    }
+
+    private fun setUpAvatar() {
+        val actionBar = supportActionBar
+        Thread {
+            val bitMapImage = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val avatarImage = BitmapDrawable(resources, bitMapImage)
+            runOnUiThread {
+                actionBar?.setDisplayHomeAsUpEnabled(true)
+                actionBar?.setHomeAsUpIndicator(avatarImage)
+                actionBar?.title = auth.currentUser?.displayName
+            }
+        }.start()
     }
 }
